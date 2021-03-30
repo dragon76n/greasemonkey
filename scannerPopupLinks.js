dc8c30531f2cf1b80f1318d5f5be5f4e2423c5f0
dragon76n <nathaniel@dragonshobbies.com>
2021-03-30 16:02:27 -0400

    Update scannerPopupLinks.js

--- scannerPopupLinks.js
+++ scannerPopupLinks.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Open scanner pages in new window
 // @namespace      fasttrackdev
-// @version        0.1
+// @version        0.2
 // @author         Nathaniel "Dragon"
 // @description    Add _blank target to links that have the word "scan" in the 1st segment URL.
 // @match          http://fasttrackdev/*
@@ -12,12 +12,23 @@ function prep() {
     for(var i=0; i<links.length; i++) {
         if(links[i].href.match(pattern)) {
             links[i].target = "_blank";
+            links[i].classList.add("scanner-link");
+            var temp = links[i].href;
             arr.push(links[i].href);
             console.log(links[i].href);
+            links[i].dataset.href = temp;
+            links[i].href = '#';
+            links[i].removeAttribute('href');
+            links[i].addEventListener("click", popup);
         }
     }
+
     console.log('There are ' + arr.length + ' matching links on this page.');
     return arr;
+}
+
+function popup() {
+    window.open(this.dataset.href, '_blank', 'width=320,height=240');
 }
 
 prep();
